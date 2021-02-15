bf0d34e438b76aa3379ea13f507b5ee7bbcf7aff
Senyang Yu <dolly@mail.ustc.edu.cn>
2021-02-15 11:35:51 +0800

    update

---
+++ redis_stream_demo/src/test/java/com/dolly/com/redis_stream_demo/RedisStreamDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dolly.com.redis_stream_demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RedisStreamDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
